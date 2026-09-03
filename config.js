85c732d88024f38c188335b976cb9ca476bb441a
indhujasarees-bit <indhujasarees@gmail.com>
2026-09-03 12:14:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.INDHUJA_CONFIG={
+  SUPABASE_URL:"https://qqujjgpzmngjogkbiynt.supabase.co",
+  SUPABASE_KEY:"sb_publishable_p3zTNlO4bppGA3jwq9avqQ_8nMjcIEt",
+  WHATSAPP_NUMBER:"919384067311",
+  SHOP_NAME:"Indhuja Saree's",
+  LOCATION:"Ilampillai, Salem",
+  INSTAGRAM_URL:"https://www.instagram.com/indhujachinraj?igsi=MTF2NzgzOXNkcTV1ZQ==",
+  FACEBOOK_URL:"https://www.facebook.com/share/1Q7Yv6K7vt/"
+};
